a884a5c5a4cbf8c82474485f9aa0913f2ddd925a
Max-codestore <116363309+Max-codestore@users.noreply.github.com>
2023-01-13 14:47:18 +0000

    Update graphics.py
    worked more on the graphical display, now you can actually run from fights

--- graphics.py
+++ graphics.py
@@ -1,6 +1,6 @@
 import random
 import pygame
-import main
+import main,time
 # Global Variables
 COLOR = (255, 100, 98)
 SURFACE_COLOR = (167, 255, 100)
@@ -8,10 +8,13 @@ WIDTH = 500
 HEIGHT = 500
 current="images/east_plain.png"
 previous =''
+now = False
+old = ''
+first=False
 #((main.L.loc(main.M.location).index(main.L.precise_location))) + 2 ==len(main.L.loc(main.M.location)) and  main.L.unlocked[(main.L.map.index(main.M.location)) + 1] != 1:
 #checking if the area is enabled or not
 def movement(direction,nx,ny,move):
-    global current,encounter,previous
+    global current,encounter,previous,now,old,bg,first
     x=playerCar.rect.x
     y=playerCar.rect.y
     z=main.L.move(main.M.location)
@@ -46,37 +49,80 @@ def movement(direction,nx,ny,move):
         playerCar.rect.x = nx
         main.L.precise_location = z[0]
     elif encounter[0] == True:
-        previous = current
+        if first == True:
+            old = current
+            previous = current
+            first = False
         bg=('images/battle_bg.png')
         current=bg
+        now = True
+    elif now == True and encounter[0] == False:
+        now = False
+        current=previous
+        bg=current
+        return current
     else:
+        first = True
         print(current)
         return current
     return bg
-def hpbar(current,max,length):
-    hpratio = length/max
-    print(current)
-    x = current//hpratio
-    pygame.draw.rect(screen,(255,0,0),(10,10,x,25))
-#    pygame.draw.rect(screen,(255,255,255),(10,10,length,25))
-def battle():
-    global screen,current,encounter
-    all_sprites_list.remove(playerCar)
-    hpbar(encounter[1][0],encounter[1][0],1000)
-    if keys[pygame.K_LEFT]:
-        encounter[1][0] -= 30
-    print(encounter[0])
-    if encounter[1][0] < 0:
-        x = list(encounter)
-        x[0] = False
+class Battle:#finish class
+    def __init__(self):
+        self.x, self.y = pygame.mouse.get_pos()
+        self.run_texture = Sprite_button_image('images/Run.png',374,381)
+        self.runbutton = Sprite_button((0,23,0),35,94,'run',374,381)
+        barriers.add(self.runbutton)
+        self.fight_texture = Sprite_button_image('images/Fight.png',74,381)
+        self.fight_button = Sprite_button((0,0,0),41,97,'fight',74,381)
+        barriers.add(self.fight_button)
+    def hpbar(self,current,max,length):
+        hpratio = length/max
+        x = current//hpratio
+        pygame.draw.rect(screen,(255,0,0),(10,10,x,25))
+    def battle(self):
+        global screen,current,encounter,start,starthp,action
+        run = False
+        all_sprites_list.remove(playerCar)
+        b=self.fight_button.collide()
+        if b == '':
+            b=self.runbutton.collide()
+        if b == 'fight':
+            self.fight_UI(action,b)
+        if b == 'run':
+            run = main.B.run()
+        if start == True:
+            starthp = encounter[1][0]
+        self.hpbar(encounter[1][0],starthp,1000)
+        if encounter[1][0] <= 0 or run == True:
+            x = list(encounter)
+            x[0] = False
+            x[1][0] = starthp
+            all_sprites_list.add(playerCar)
+            return x
+        return encounter
+    def fight_UI(self,action,buttons):
+        self.attack_texture = Sprite_button_image('images/Attack.png',374,381)
+        self.attack_button = Sprite_button((0,23,0),35,94,'Attack',374,381)
+        barriers.add(self.attack_button)
+        self.heal_texture = Sprite_button_image('images/Heal.png',74,381)
+        self.heal_button = Sprite_button((0,0,0),41,97,'heal',74,381)
+        barriers.add(self.heal_button)
+        self.magic_texture = Sprite_button_image('images/magic.png',220,381)
+        self.magic_button = Sprite_button((0,0,0),41,97,'magic',220,381)
+        barriers.add(self.magic_button)
+        return True
+    def battle_UI(sef,pressed):
+        print('s')
+ #       if pressed != True:
 
+#            return runbutton,fight_button
 
 def map_collision(current,dire):
     if current == 'images/east_plain.png':
         tela1 = telaport((0,0,0),1,500,0,0,'forward',playerCar.rect.x,470)
         barriers.add(tela1)
         tela1.collide()
-    if current == 'images/south_plain.png':#fix the movement point location
+    if current == 'images/south_plain.png':
         tela4 = telaport((0,0,0),500,1,0,0,'backward',450,playerCar.rect.y)
         barriers.add(tela4)
         tela4.collide()
@@ -141,7 +187,6 @@ class Sprite(pygame.sprite.Sprite):
         pygame.draw.rect(self.image,
                          color,
                          pygame.Rect(0, 0, width, height))
-
         self.rect = self.image.get_rect()
         self.rect.y=50
         self.speed = 0
@@ -175,18 +220,46 @@ class Sprite_NPC(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x= 100
         self.rect.y=200
-        self.queest = questgive
+        self.quest = questgive
     def get_quest(self):
-        print(self.queest)
+        print(self.quest)
     def collide(self):
         if playerCar.rect.x == self.rect.x and playerCar.rect.y == self.rect.y:
-            print(self.queest)
+            print(self.quest)
+
+class Sprite_button(pygame.sprite.Sprite):
+    def __init__(self, color, height, width,functons,x,y):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(SURFACE_COLOR)
+        self.image.set_colorkey(COLOR)
+
+        pygame.draw.rect(self.image,
+                         color,
+                         pygame.Rect(0, 0, width, height))
+        self.width = width
+        self.rect = self.image.get_rect()
+        self.function = functons
+        self.mouse = pygame.mouse.get_pos()
+        self.rect.x = x
+        self.rect.y = y
+    def collide(self):
+        if (self.mouse[0] >= self.rect.x and self.mouse[0] <= self.rect.x+self.width) and (self.mouse[1] >= self.rect.y and self.mouse[1] <= self.rect.y+self.width) and pygame.mouse.get_pressed()[0] == True:
+            print(self.function)
+            return self.function
+        return ''
 class Sprite_enermy(pygame.sprite.Sprite):
     def __init__(self, enermy):
         super().__init__()
-        global current
         spite = pygame.image.load('images/enermy.png')
         screen.blit(spite,(250,250))
+class Sprite_button_image(pygame.sprite.Sprite):
+    def __init__(self, button_image,x,y):
+        super().__init__()
+        global current
+        spite = pygame.image.load(button_image)
+        screen.blit(spite,(x,y))
 class telaport(pygame.sprite.Sprite):
     def __init__(self, color, height, width,x,y,forward,posx,posy):
         super().__init__()
@@ -255,6 +328,10 @@ screen2 = False
 resentdirection = ''
 encounter = False,0
 imp=''
+start = True
+starthp=3
+action = ''
+
 while exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -281,21 +358,40 @@ while exit:
             playerCar.moveBack(10)
             resentdirection = '-y'
             encounter = main.B.encounter(False)
+        if keys[pygame.K_m]:
+            time.sleep(1)
+            imp = pygame.image.load(f'images/{main.M.location}_map.png')
+            screen.blit(imp,(0,0))
+            all_sprites_list.draw(screen)
+            pygame.display.flip()
+            time.sleep(10)
+            #something for future, put player sprite on the section of the map they are in
+            if keys[pygame.K_m] == True:
+                imp = pygame.image.load(movement('', 0, 0, False))
+            else:
+                time.sleep(2)
     imp = pygame.image.load(movement('', 0, 0, False))
-    mouse = pygame.mouse.get_pos()
     map_collision(current,resentdirection)
     all_sprites_list.update()
     screen.blit(imp, (0, 0))
     if encounter[0] == True:
         enermy = Sprite_enermy('theif')
-        battle()
-    one_NPC.collide()
-#    print('x{0}'.format(playerCar.rect.x))
-#    print('y{0}'.format(playerCar.rect.y))
-    all_sprites_list.draw(screen)
-    pygame.display.flip()
-    clock.tick(60)
-
+        B = Battle()
+        encounter = B.battle()
+        start = False
+        one_NPC.collide()
+        #    print('x{0}'.format(playerCar.rect.x))
+        #    print('y{0}'.format(playerCar.rect.y))
+        all_sprites_list.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+    else:
+        start=True
+        one_NPC.collide()
+        #    print('x{0}'.format(playerCar.rect.x))
+        #    print('y{0}'.format(playerCar.rect.y))
+        all_sprites_list.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+#if event.type == pygame.MOUSEBUTTONDOWN:
 pygame.quit()
-
-#x250y10- x330-y10,x350-y110, x330, y460-x250,y460, x250,y270
